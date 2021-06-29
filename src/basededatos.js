9f869ae2d0572c352ef1102b1d2b803bf67b5001
ema-munoz <ema.munoz@yavirac.edu.ec>
2021-06-29 08:16:09 -0500

    29/06/2021

--- src/basededatos.js
+++ src/basededatos.js
@@ -1,30 +1,16 @@
+const Sequelize = require('sequelize')
 
-const mysql = require('mysql');
-
-const {promisify} = require('util');
-
-const {database} = require('./key');
-
-const pool = mysql.createPool (database);
-
-pool.getConnection ((err,connection)=>{
-    if (err){
-        if (err.coder === "PROTOCOL_CONNECTION_LOST"){
-            console.error ("Se cerró la conexión la base de datos")
-        }
-        if (err.coder === "ER_CON_COUNT_ERROR"){
-            console.error ("La base de datos tiene muchas conexiones")
-        }
-        if (err.coder === "ECONNREFUSED"){
-            console.error ("Conexión a base de datos rechazada")
-        }
-    }
-
-    if (connection) connection.release ()
-        console.log ("La base de datos esta conectada")
-    return 
+const sequelize = new Sequelize('Epilepsum', 'root', '', {
+  host: 'localhost',
+  dialect: 'mysql',
 })
 
-pool.query = promisify (pool.query)
+sequelize.authenticate()
+  .then(() => {
+    console.log('Conectado')
+  })
+  .catch(err => {
+    console.log('No se conecto')
+  })
 
-module.exports = pool
+  module.exports = sequelize
